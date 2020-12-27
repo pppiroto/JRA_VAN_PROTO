e9018dd731d00c009b5c7f2f3c21de2dcfbf9a8b
矢木 浩人 <pppiroto@hotmail.com>
2020-12-27 11:53:22 +0900

    Open ブロッキング以外

--- JRA_VAN_PROTO/Form1.Designer.cs
+++ JRA_VAN_PROTO/Form1.Designer.cs
@@ -29,25 +29,37 @@ namespace JRA_VAN_PROTO
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnThisWeek = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSettings
             // 
-            this.button1.Location = new System.Drawing.Point(147, 174);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSettings.Location = new System.Drawing.Point(12, 12);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 23);
+            this.btnSettings.TabIndex = 0;
+            this.btnSettings.Text = "設定";
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // btnThisWeek
+            // 
+            this.btnThisWeek.Location = new System.Drawing.Point(93, 12);
+            this.btnThisWeek.Name = "btnThisWeek";
+            this.btnThisWeek.Size = new System.Drawing.Size(75, 23);
+            this.btnThisWeek.TabIndex = 1;
+            this.btnThisWeek.Text = "今週開催";
+            this.btnThisWeek.UseVisualStyleBackColor = true;
+            this.btnThisWeek.Click += new System.EventHandler(this.btnThisWeek_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnThisWeek);
+            this.Controls.Add(this.btnSettings);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -57,7 +69,8 @@ namespace JRA_VAN_PROTO
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnThisWeek;
     }
 }
 
--- JRA_VAN_PROTO/Form1.cs
+++ JRA_VAN_PROTO/Form1.cs
@@ -21,20 +21,21 @@ namespace JRA_VAN_PROTO
         {
             resources = new System.Resources.ResourceManager(this.GetType());
             jVLink.Location = new System.Drawing.Point(0, 0);
-            jVLink.Name = "jVLink";
+            jVLink.Name = "jVLink"; 
             jVLink.OcxState = (System.Windows.Forms.AxHost.State)resources.GetObject($"{jVLink.Name}.OcxState");
             jVLink.Size = new System.Drawing.Size(10, 10);
             this.Controls.Add(jVLink);
 
             InitializeComponent();
-
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnSettings_Click(object sender, EventArgs e)
         {
-
-            jVLink.JVSetUIProperties();
-            
+            // 結果が-100 の場合は設定が何らかのエラーでレジストリに反映できなかった
+            if (this.jVLink.JVSetUIProperties() == -100)
+            {
+                MessageBox.Show("JV-Link 設定エラー");
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -48,6 +49,118 @@ namespace JRA_VAN_PROTO
             {
                 Debug.WriteLine(exception);
             }
+        }
+
+        private void btnThisWeek_Click(object sender, EventArgs e)
+        {
+            // データ読み出し処理の一般的な流れ
+
+            int returnCode = 0;
+            int readCount = 0;
+            int downloadCount = 0;
+
+            try
+            {
+                // ①JVLinkﾉ初期化(JVInit)
+                // JVLinkの初期化を行います。JVOpenを呼び出す前に最低1回呼び出す必要があります。 
+                returnCode = this.jVLink.JVInit("UNKNOW");
+                if (returnCode < 0)
+                {
+                    MessageBox.Show($"JVInitエラー。 RC={returnCode}");
+                    return;
+                }
+
+
+                // ②JVDataﾉｵｰﾌﾟﾝ(JVOpen) 
+                // 要求に応じたデータを読み出せるように準備します。
+                // 必要であればサーバーからデータをダウンロードする処理を起動します。
+                // JVOpenからの戻りとしてダウンロードを行なう予定ファイル数が返されます。 
+
+                string dataSpec = "RACE";               // データ種別に「レース情報」を設定
+                string fromTime = "20201201000000";    // Fromタイムに2020年12月1日を設定
+                int optionFlag = 2;                     // オプションに「今週データ」を設定
+                returnCode = this.jVLink.JVOpen(dataSpec, fromTime, optionFlag, ref readCount, ref downloadCount, out string lastFileTimestamp);
+                if (returnCode < 0)
+                {
+                    MessageBox.Show($"JOpenエラー。 RC={returnCode}");
+                    return;
+                }
+
+                if (readCount == 0)
+                {
+                    MessageBox.Show($"該当データがありません。");
+                    return;
+                }
+
+                // ③ﾀﾞｳﾝﾛｰﾄﾞ中ﾉﾌﾟﾛｸﾞﾚｽﾊﾞｰ表示(JVStatus)
+                // JVOpenはダウンロードスレッドを起動するとダウンロード完了前に呼び出し側に制御を返しますので、
+                // 呼び出し側はJVStatusを使ってダウンロード処理の進行状況を監視する必要があります。
+                // JVStatusはダウンロード済みのファイル数を返しますので、JVOpen時のダウンロード予定ファイル数と一致した場合にダウンロード処理の完了とします。 
+
+                // TODO
+
+
+
+
+
+
+
+
+                // ④JVDataﾉ読ﾐ出ｼ(JVRead)
+                // データを読み出します。ダウンロードスレッドがダウンロード実行中にJVReadを呼び出すとエラーとなります。
+
+                string recordSpec = null;
+                JVData_Struct.JV_RA_RACE race = new JVData_Struct.JV_RA_RACE();
+                string buff = null;
+                int buffSize = 1500;
+                string fName = null;
+
+                buff = new string('\0', buffSize);
+                if (readCount > 0)
+                {
+                    while ((returnCode = this.jVLink.JVRead(out buff, out buffSize, out fName)) != 0)
+                    {
+
+                        if (returnCode > 0)
+                        {
+                            recordSpec = buff.Substring(1, 2);
+                            if (recordSpec == "RA")
+                            {
+                                race.SetDataB(ref buff);
+                            }
+                            else
+                            {
+                                Debug.WriteLine($"{race.id.Year}{race.id.MonthDay} {race.id.JyoCD} {race.id.Kaiji} {race.id.Nichiji} {race.id.RaceNum} {race.RaceInfo.Hondai}");
+                            }
+                        }
+                        else if (returnCode == -1)
+                        {
+
+                        }
+                        else
+                        {
+                            MessageBox.Show($"読み込みエラー。 RC={returnCode}");
+                            return;
+                        }
+                    }
+                }
+
+
+                // ⑤ﾃﾞｰﾀ内容ｦﾃｷｽﾄﾎﾞｯｸｽﾆ表示
+                // 一般の競馬ソフトではこの処理の代わりにデータベースへデータを反映する処理が行なわれます。
+
+            } 
+            finally
+            {
+                // ⑥JVDataﾉｸﾛｰｽﾞ(JVClose) 
+                // JVOpenで確保されたリソースを開放します。
+                if (downloadCount > 0)
+                {
+
+                }
+                returnCode = this.jVLink.JVClose();
+            }
+
         }
     }
 }
